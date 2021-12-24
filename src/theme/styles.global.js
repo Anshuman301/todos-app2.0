57af947960de0d8a24f804b313986dd803965e2d
Anshuman301 <anshumanprsd@gmail.com>
2021-12-25 02:18:28 +0530

    change % of bgGradient

--- src/theme/styles.global.js
+++ src/theme/styles.global.js
@@ -7,7 +7,7 @@ const styles =  {
             },
             body: {
                 color: props.colorMode === 'light' ? 'green.700' : 'blue.600',
-                bgGradient: props.colorMode === 'light' ? 'linear(to-t, green.200, green.500)' : 'linear(to-tr, gray.800, gray.900)'
+                bgGradient: props.colorMode === 'light' ? 'linear(to-t, green.200, green.500)' : 'linear(to-tr, gray.800 50%, gray.900 25%)'
             },
             '.globalSideBg': {
                 bgGradient: props.colorMode === 'light' ? 'linear(to-tr, orange.100, orange.200)' : 'linear(to-t, teal.900, teal.800)'
